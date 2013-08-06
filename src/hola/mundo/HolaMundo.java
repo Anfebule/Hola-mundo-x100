9eabf286d7ec368f60c728c87e0fff77a1b1aee3
Anfebule <i_has_you@hotmail.com>
2013-08-06 08:30:40 -0500

    nuevo repositorio

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 201
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for (int i=1;i<100;i++)
+       {
+           System.out.println("No volveré a hacer las cosas fáciles");
+           
+       }
+    }
+}
